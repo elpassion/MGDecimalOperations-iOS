c23b152ad97165926ff0ececb1f950561e09fbcc
Maciej Gomółka <OPQ@MacBook-Pro-Maciej-3.local>
2016-08-24 10:49:08 +0200

    Initial commit

---
+++ MGDecimalOperations/MGDecimalOperations.h
@@ -0,0 +1,14 @@
+//
+//  Created by Maciej Gomółka on 24.08.2016.
+//  Copyright © 2016 EL Passion. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MGDecimalOperations.
+FOUNDATION_EXPORT double MGDecimalOperationsVersionNumber;
+
+//! Project version string for MGDecimalOperations.
+FOUNDATION_EXPORT const unsigned char MGDecimalOperationsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MGDecimalOperations/PublicHeader.h>
---
+++ MGDecimalOperationsTests/MGDecimalOperationsTests.m
@@ -0,0 +1,18 @@
+//
+//  Created by Maciej Gomółka on 24.08.2016.
+//  Copyright © 2016 EL Passion. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface MGDecimalOperationsTests : XCTestCase
+@end
+
+@implementation MGDecimalOperationsTests
+
+- (void)testExample {
+    NSInteger result = 1 + 1;
+    XCTAssertTrue(result == 2);
+}
+
+@end
