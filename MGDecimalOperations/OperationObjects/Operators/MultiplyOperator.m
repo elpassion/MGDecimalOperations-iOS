d098c0219c46df2f0dc463da74398c7b478d6713
Maciej Gomółka <maciej.gomolka@elpassion.pl>
2016-08-30 11:25:14 +0200

    Add MultiplyOperator

---
+++ MGDecimalOperations/OperationObjects/Operators/MultiplyOperator.m
@@ -0,0 +1,27 @@
+//
+// Created by Maciej Gomółka on 28.08.2016.
+// Copyright (c) 2016 EL Passion. All rights reserved.
+//
+
+#import "MultiplyOperator.h"
+#import "Variable.h"
+
+@implementation MultiplyOperator
+
+- (instancetype)init
+{
+    if (self = [super init]) {
+        _priority = 2;
+        _symbol = @"*";
+    }
+    return self;
+}
+
+- (Variable *)makeOperationWithFirstArgument:(Variable *)firstArgument SecondArgument:(Variable *)secondArgument
+{
+    Variable *result = [[Variable alloc] initWithSymbol:@"" value:@"0"];
+    result.value = [firstArgument.value decimalNumberByMultiplyingBy:secondArgument.value];
+    return result;
+}
+
+@end
