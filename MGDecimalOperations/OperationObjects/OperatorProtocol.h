a26315fc0248027ff14a47b9acaf8f81e65d39d9
Maciej Gomółka <maciej.gomolka@elpassion.pl>
2016-08-30 11:09:45 +0200

    Add OperatorProtocol
    Implemented by all operators

---
+++ MGDecimalOperations/OperationObjects/OperatorProtocol.h
@@ -0,0 +1,17 @@
+//
+// Created by Maciej Gomółka on 30.08.2016.
+// Copyright (c) 2016 EL Passion. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "OperationObjectProtocol.h"
+
+@class Variable;
+
+@protocol OperatorProtocol <NSObject, OperationObjectProtocol>
+
+@property (nonatomic) NSInteger priority;
+
+- (Variable *)makeOperationWithFirstArgument:(Variable *)firstArgument SecondArgument:(Variable *)secondArgument;
+
+@end
