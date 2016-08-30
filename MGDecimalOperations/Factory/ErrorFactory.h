8e61fce4eb7901747c8652965df488e43213e54b
Maciej Gomółka <maciej.gomolka@elpassion.pl>
2016-08-30 11:33:49 +0200

    Add ErrorFactory

---
+++ MGDecimalOperations/Factory/ErrorFactory.h
@@ -0,0 +1,12 @@
+//
+// Created by Maciej Gomółka on 24.08.2016.
+// Copyright (c) 2016 EL Passion. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ErrorFactory : NSObject
+
+- (NSError *)errorWithMessage:(NSString *)errorMessage;
+
+@end
---
+++ MGDecimalOperations/Factory/ErrorFactory.m
@@ -0,0 +1,17 @@
+//
+// Created by Maciej Gomółka on 24.08.2016.
+// Copyright (c) 2016 EL Passion. All rights reserved.
+//
+
+#import "ErrorFactory.h"
+
+@implementation ErrorFactory
+
+- (NSError *)errorWithMessage:(NSString *)errorMessage
+{
+    NSMutableDictionary *details = [NSMutableDictionary new];
+    details[NSLocalizedDescriptionKey] = errorMessage;
+    return [NSError errorWithDomain:@"MGDecimalOperations" code:1 userInfo:nil];
+}
+
+@end
