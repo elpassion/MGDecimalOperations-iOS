ce11b47433681aa840604c11bad9c3f9cb637cf0
Maciej Gomółka <maciej.gomolka@elpassion.pl>
2016-08-30 11:00:42 +0200

    Add OperationObjectProtocol
    Implemented by all operation objects

---
+++ MGDecimalOperations/OperationObjects/OperationObjectProtocol.h
@@ -0,0 +1,12 @@
+//
+// Created by Maciej Gomółka on 30.08.2016.
+// Copyright (c) 2016 EL Passion. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@protocol OperationObjectProtocol <NSObject>
+
+@property (nonatomic, strong) NSString *symbol;
+
+@end
