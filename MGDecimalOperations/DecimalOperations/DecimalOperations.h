33a7c40f2eeac200ef8176da403121279cd934cb
Maciej Gomółka <maciej.gomolka@elpassion.pl>
2016-09-13 12:02:29 +0200

    Add documentation to public headers

--- MGDecimalOperations/DecimalOperations/DecimalOperations.h
+++ MGDecimalOperations/DecimalOperations/DecimalOperations.h
@@ -7,9 +7,44 @@
 
 @class Variable;
 
+/**
+ *  The 'DecimalOperations' class is responsible for execution 
+ *  operations on NSDecimalNumbers wrote in String.
+ */
+
 @interface DecimalOperations : NSObject
 
+/**
+ *  This is method for calculate operations wrote in String. 
+ *  In this method you provide to the variables parameter NSDictionary
+ *  contains keys with name of variables and numbers wrote in String that
+ *  will be converted to NSDecimalNumbers.
+ *
+ *  @param operation is operation wrote in String
+ *  @param variables is NSDictionary where the key is name of variable that represent your number in String operation
+ *                   and value is number in String that will be converted to the NSDecimalNumber.
+ *  @param error     is NSError. Before you call this method you need to create NSError pointer that you pass.
+ *                   When error occurs this variable will describe problem.
+ *
+ *  @return NSDeciamlNumber that is result of operation wrote in @parm operation
+ */
+
 - (NSDecimalNumber *)mathWithOperation:(NSString *)operation variablesString:(NSDictionary *)variables error:(NSError **)error;
+
+/**
+ *  This is method for calculation operations wrote in String.
+ *  In this method you provide to the variables parametr NSDictionary
+ *  contains keys with name of variables and NSDecimalNumbers
+ *
+ *  @param operation is operation wrote in String
+ *  @param variables is NSDictionary where the key is name of variable that represent
+ *                   your number in String operation and value is NSDecimalNumber.
+ *  @param error     is NSError. Before you call this method you need to create NSError
+ *                   pointer that you pass. When error occurs this variable will describe problem.
+ *
+ *  @return NSDeciamlNumber that is result of operation wrote in @parm operation
+ */
+
 - (NSDecimalNumber *)mathWithOperation:(NSString *)operation variablesDecimal:(NSDictionary *)variables error:(NSError **)error;
 
 @end
