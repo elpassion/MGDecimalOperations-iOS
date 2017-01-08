0d97b435ad00a8c659c7eaa463e1b64368fb0a39
Maciej Gomółka <maciej.gomolka@elpassion.pl>
2017-01-08 03:50:17 +0100

    Add NonValueObjectProtocol

---
+++ MGDecimalOperations/OperationObjects/NonValueObjectProtocol.h
@@ -0,0 +1,9 @@
+//
+// Created by Maciej Gomółka on 08.01.2017.
+// Copyright (c) 2017 EL Passion. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@protocol NonValueObjectProtocol <NSObject>
+@end
