5d5d267e3bda09522f823aa49176f6a7865c41ea
Mateusz Szklarek <mateusz.szklarek@elpassion.pl>
2017-01-26 08:55:33 +0100

    Refactor DecimalConverter implementation

--- MGDecimalOperations/Converters/DecimalConverter.m
+++ MGDecimalOperations/Converters/DecimalConverter.m
@@ -23,10 +23,9 @@
     NSArray *keys = variables.allKeys;
     for (NSUInteger i = 0; i < variables.count; i++) {
         NSString *key = keys[i];
-        NSString *number = variables[key];
-        if ([self isCorrectNumber:number]) {
-            NSDecimalNumber *decimalNumber = [self decimalNumberFromString:number];
-            decimalVariables[key] = decimalNumber;
+        NSString *stringNumber = variables[key];
+        if ([self isCorrectNumber:stringNumber]) {
+            decimalVariables[key] = [[NSDecimalNumber alloc] initWithString:stringNumber];
         } else {
             *error = [self.errorFactory errorWithMessage:@"Argument not a number"];
             return nil;
@@ -35,20 +34,23 @@
     return decimalVariables;
 }
 
-- (NSDecimalNumber *)decimalNumberFromString:(NSString *)string
-{
-    return [[NSDecimalNumber alloc] initWithString:string];
-}
-
 - (BOOL)isCorrectNumber:(NSString *)variable
 {
-    NSUInteger dotNumber = 0;
+    NSUInteger numberOfDots = 0;
     for (NSUInteger i = 0; i < variable.length; i++) {
         UniChar character = [variable characterAtIndex:i];
-        if (character == 46) dotNumber++;
-        if ((character < 48 || character > 57) && character != 46) return NO;
+        if (character == '.') {
+            numberOfDots++;
+        } else if ([self isOutOfRange:character]) {
+            return NO;
+        }
     }
-    return dotNumber <= 1;
+    return numberOfDots <= 1;
+}
+
+- (BOOL)isOutOfRange:(UniChar)character
+{
+    return character < '0' || character > '9';
 }
 
 @end
