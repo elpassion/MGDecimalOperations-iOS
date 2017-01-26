987246b7927a15bfaa7110223fc6349d2e4e113d
Mateusz Szklarek <mateusz.szklarek@elpassion.pl>
2017-01-26 11:54:45 +0100

    Refactor ErrorFactory implementation

--- MGDecimalOperations/Factory/ErrorFactory.m
+++ MGDecimalOperations/Factory/ErrorFactory.m
@@ -4,9 +4,10 @@
 
 - (NSError *)errorWithMessage:(NSString *)errorMessage
 {
-    NSMutableDictionary *details = [NSMutableDictionary new];
-    details[NSLocalizedDescriptionKey] = errorMessage;
-    return [NSError errorWithDomain:@"MGDecimalOperations" code:1 userInfo:[details copy]];
+    NSDictionary *details = @{
+            NSLocalizedDescriptionKey: errorMessage
+    };
+    return [NSError errorWithDomain:@"MGDecimalOperations" code:1 userInfo:details];
 }
 
 @end
